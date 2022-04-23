496135ec08cad1e62c3d00e71772f5ba7a208260
Karma Blackshaw <irenesejah29@gmail.com>
2022-04-23 16:52:38 +0800

    add base component importer

--- src/main.js
+++ src/main.js
@@ -7,7 +7,25 @@ import store from './store'
 
 import './assets/scss/app/_@index.scss'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app')
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const app = createApp(App)
+
+const requireComponent = require.context('./components',true,/Base[A-Z]\w+\.(vue|js)$/)
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName)
+
+  // Get PascalCase name of component
+  const componentName = upperFirst(camelCase(fileName.split('/').pop().replace(/\.\w+$/, '')))
+
+  app.component(componentName, componentConfig.default)
+})
+
+app.use(store)
+
+app.use(router)
+
+app.mount('#app')
