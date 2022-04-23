ad7c71a6e383bf38e1fbc5fb1e9400f4a4c16e1b
Karma Blackshaw <irenesejah29@gmail.com>
2022-04-23 23:01:42 +0800

    add more images to services

--- src/views/services/ServicesView.vue
+++ src/views/services/ServicesView.vue
@@ -40,7 +40,12 @@ export default {
     const images = reactive([
       'https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=1600',
       'https://images.pexels.com/photos/5015503/pexels-photo-5015503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      'https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&w=1600'
+      'https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      'https://images.pexels.com/photos/11866150/pexels-photo-11866150.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/11710660/pexels-photo-11710660.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/11384154/pexels-photo-11384154.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/11041919/pexels-photo-11041919.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/11155273/pexels-photo-11155273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     ])
 
     function getRandomImage () {
