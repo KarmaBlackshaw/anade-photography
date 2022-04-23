76bedfa93f458f9d717c9aaafdf8160f3ed8c5ed
Karma Blackshaw <irenesejah29@gmail.com>
2022-04-23 20:46:31 +0800

    apply services page

--- src/views/hero/HeroView.vue
+++ src/views/hero/HeroView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="hero">
-    <HomeView />
-  </div>
+  <section class="hero">
+    <HomeView id="home-view" />
+
+    <div class="container mx-auto px-10 sm:px-20">
+      <FlowView
+        id="flow-view"
+        class="hero__item--flow"
+      />
+
+      <ServicesView id="services-view" />
+
+      <!-- <AboutView id="about-view" /> -->
+    </div>
+  </section>
 </template>
 
 <script>
-import HomeView from '@/views/home/HomeView.vue'
+import HomeView from '@/views/home/HomeView'
+import FlowView from '@/views/flow/FlowView'
+import ServicesView from '@/views/services/ServicesView'
+import AboutView from '@/views/about/AboutView'
+
 export default {
   components: {
-    HomeView
+    HomeView,
+    AboutView,
+    FlowView,
+    ServicesView
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import './assets/scss/HeroView';
+@import './assets/scss/HeroView.scss';
 </style>
