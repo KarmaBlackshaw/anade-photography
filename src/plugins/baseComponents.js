f59f73f76fd88643eb3bf5a715779448dd760ccd
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-06 13:47:17 +0800

    apply base components plugin

---
+++ src/plugins/baseComponents.js
@@ -0,0 +1,48 @@
+/**
+
+Usage:
+
+import baseComponents from '@/plugins/baseComponents'
+
+app.use(baseComponents, options)
+
+Options:
+- path     {String}      // The relative path of the components folder
+- deep     {Boolean}     // Whether or not to look in subfolders
+- regex    {Regex}       // The regular expression used to match base component filenames
+
+ */
+
+// libs
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+// helpers
+const pipe = (initial, fns) => fns.reduce((v, f) => f(v), initial)
+
+export default {
+  install: app => {
+    const requireComponent = require.context(
+      '../components',
+      true,
+      /Base[A-Z]\w+\.(vue|js)$/
+    )
+
+    requireComponent.keys().forEach(fileName => {
+      // Get component config
+      const componentConfig = requireComponent(fileName)
+
+      // Get PascalCase name of component
+      const componentName = pipe(fileName, [
+        val => val.split('/'),
+        val => val.pop(),
+        val => val.replace(/\.\w+$/, ''),
+        val => camelCase(val),
+        val => upperFirst(val)
+      ])
+
+      // Register component globally
+      app.component(componentName, componentConfig.default)
+    })
+  }
+}
