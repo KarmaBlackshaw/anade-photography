c778b62d2ecea61ee827a5747ec4c1f2ad4bbaca
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-06 14:36:27 +0800

    update home view content

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -71,7 +71,7 @@
         </div>
 
         <div class="introduction__subtitle mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, iusto nesciunt, autem eaque quae id obcaecati ullam ab quibusdam veniam quaerat architecto sapiente quia temporibus! Placeat expedita amet vero in!
+          Experience photography like you've never been before.
         </div>
 
         <div class="introduction__actions">
