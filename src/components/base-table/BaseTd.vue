39be23d9a0df96b550e2dc76de155ad2bd8d1d3e
karmablackshaw <irenesejah29@gmail.com>
2022-05-28 18:24:17 +0800

    feat: width props for td

--- src/components/base-table/BaseTd.vue
+++ src/components/base-table/BaseTd.vue
@@ -1,11 +1,33 @@
 <template>
-  <td class="px-6 py-4">
+  <td
+    class="px-6 py-4"
+    :style="style"
+  >
     <slot></slot>
   </td>
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
-  name: "BaseTd"
+  name: "BaseTd",
+
+  props: {
+    width: {
+      type: [Number, String],
+      default: 'auto'
+    }
+  },
+
+  setup (props) {
+    const style = computed(() => {
+      return {
+        width: `${props.width}!important`
+      }
+    })
+
+    return { style }
+  }
 }
 </script>
