ee8d63acac592d94b325675a85793f8a6c09ee11
karmablackshaw <irenesejah29@gmail.com>
2022-05-28 18:23:03 +0800

    ref: about mgmt modal usage

--- src/views/about-management/AboutMgmt.vue
+++ src/views/about-management/AboutMgmt.vue
@@ -12,10 +12,13 @@
         <base-table class="about-td">
           <base-thead>
             <base-th>Title</base-th>
+
             <base-th>Content</base-th>
+
             <base-th class="td__thumbnail">
               Thumbnail
             </base-th>
+
             <base-th class="td__actions" />
           </base-thead>
           <base-tbody>
@@ -23,37 +26,66 @@
               <base-td>
                 Lorem ipsum dolor sit
               </base-td>
+
               <base-td class="truncate">
-                amet consectetur adipisicing elit. Aspernatur exercitationem officia, aut soluta expedita ipsam delectus nostrum sit porro excepturi esse ipsum dolorum sequi perspiciatis, explicabo id officiis. Voluptate, mollitia.
+                {{ truncateString('amet consectetur adipisicing elit. Aspernatur exercitationem officia, aut soluta expedita ipsam delectus nostrum sit porro excepturi esse ipsum dolorum sequi perspiciatis, explicabo id officiis. Voluptate, mollitia.', 30) }}
               </base-td>
+
               <base-td class="td__thumbnail">
                 <img
                   src="https://images.pexels.com/photos/10334838/pexels-photo-10334838.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                   alt=""
                 >
               </base-td>
-              <base-td class="td__actions">
-                <img
-                  src="@/assets/images/edit.svg"
-                  alt=""
-                >
-                <img
-                  src="@/assets/images/trash-2.svg"
-                  alt=""
-                >
+
+              <base-td
+                class="td__actions"
+                width="20px"
+              >
+                <base-icon-edit
+                  class="actions__icon"
+                  @click="modal = !modal"
+                />
+
+                <base-icon-trash class="actions__icon" />
               </base-td>
             </base-tr>
           </base-tbody>
         </base-table>
       </base-card-body>
     </base-card>
+
+    <base-modal v-model="modal">
+      <template #header>
+        header
+      </template>
+      asfasdf
+      <template #footer>
+        footer
+      </template>
+    </base-modal>
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
+  setup () {
+    const truncateString = (string, maxLength) => {
+      return string.length > maxLength
+        ? `${string.substring(0, maxLength)}…`
+        : string
+    }
 
+    const modal = ref(false)
+
+    return {
+      modal,
+
+      truncateString
+    }
+  }
 }
 </script>
 
