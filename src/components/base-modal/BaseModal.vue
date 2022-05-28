95f1f3345c1642617f8c2ad6aef61ebbc7c64e2c
karmablackshaw <irenesejah29@gmail.com>
2022-05-28 18:20:39 +0800

    feat: modal

---
+++ src/components/base-modal/BaseModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <Transition name="fade">
+    <div
+      v-if="modelValue"
+      class="modal-container"
+      @click.self="triggerClose"
+    >
+      <div class="modal">
+        <!-- Header -->
+        <div class="modal__header">
+          <slot name="header"></slot>
+
+          <base-icon-close
+            class="icon-close"
+            color="#F87171"
+            @click="triggerClose"
+          />
+        </div>
+
+        <!-- Content -->
+        <div class="modal__body">
+          <slot></slot>
+        </div>
+
+        <!-- Footer -->
+        <div class="modal__footer">
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: [Number, Boolean],
+      default: false
+    }
+  },
+
+  emits: ['update:modelValue'],
+
+  setup (_, ctx) {
+    function triggerClose () {
+      ctx.emit('update:modelValue', false)
+    }
+
+    return {
+      triggerClose
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/scss/BaseModal';
+</style>
