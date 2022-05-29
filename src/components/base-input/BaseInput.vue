05309100e24652e52e550ab2e55d1b75cf80b1fa
karmablackshaw <irenesejah29@gmail.com>
2022-05-30 00:01:10 +0800

    add base components for input and textarea

---
+++ src/components/base-input/BaseInput.vue
@@ -0,0 +1,37 @@
+
+<script setup>
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+
+  modelValue: {
+    type: [Number, Boolean],
+    default: false
+  }
+})
+
+</script>
+
+<template>
+  <div class="base-input-container">
+    <label
+      v-if="label"
+      class="base-input__label"
+    >
+      {{ props.label }}
+    </label>
+
+    <input
+      v-bind="$attrs"
+      type="text"
+      class="base-input"
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './assets/scss/BaseInput.scss';
+</style>
