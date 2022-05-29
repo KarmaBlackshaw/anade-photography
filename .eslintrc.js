92f90d88dc153e248d05a2d5862ae4a3582d80b8
karmablackshaw <irenesejah29@gmail.com>
2022-05-30 00:03:27 +0800

    define globals for esint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,17 @@ module.exports = {
     "es2021": true,
     "node": true
   },
-  "extends": [
+  "globals": {
+    "defineProps": true,
+    "defineEmits": true
+  },
+  "extends":
+  [
+    'plugin:vue/base',
     'plugin:vue/vue3-recommended',
-    "eslint:recommended",
-    "plugin:vue/essential"
+    'plugin:vue/vue3-strongly-recommended',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
   ],
   "parserOptions": {
     "ecmaVersion": 12,
