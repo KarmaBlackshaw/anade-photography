20903e8158d4f3a4cba99eb12e3452fe2f66555a
karmablackshaw <irenesejah29@gmail.com>
2022-06-18 17:16:03 +0800

    add base textarea v-model

--- src/components/base-textarea/BaseTextarea.vue
+++ src/components/base-textarea/BaseTextarea.vue
@@ -1,28 +1,37 @@
 
 <script setup>
 
-const props = defineProps({
+defineProps({
   label: {
     type: String,
     default: ''
+  },
+
+  modelValue: {
+    type: [String, Number],
+    default: ''
   }
 })
+
+const emit = defineEmits(['update:modelValue'])
 
 </script>
 
 <template>
   <div class="base-textarea-container">
     <label
-      v-if="props.label"
+      v-if="label"
       class="base-textarea__label"
     >
-      {{ props.label }}
+      {{ label }}
     </label>
 
     <textarea
       type="text"
       class="base-textarea"
       v-bind="$attrs"
+      :value="modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
     ></textarea>
   </div>
 </template>
